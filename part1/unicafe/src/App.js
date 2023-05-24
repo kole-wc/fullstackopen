92a8565422b8754055f965d12c274bebb41096cb
kolechr <kole.chaicharee@gmail.com>
2023-05-24 18:05:31 +0900

    Refactor code

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,34 +1,5 @@
 import { useState } from 'react';
 
-const StatisticRow = (props) => {
-  return (
-    <tbody>
-      <tr>
-        <th>{props.text}</th>
-        <td>{props.value} {props.sign}</td>
-      </tr>
-    </tbody>
-  );
-}
-
-const Statistics = (props) => {
-  if (props.total === 0) {
-    return <p>No feedback given</p>
-  }
-  return (
-    <div>
-      <table>
-        <StatisticRow text='good' value={props.good} />
-        <StatisticRow text='neutral' value={props.neutral} />
-        <StatisticRow text='bad' value={props.bad} />
-        <StatisticRow text='all' value={props.total} />
-        <StatisticRow text='average' value={props.average} />
-        <StatisticRow text='positive' value={props.positive} sign='%' />
-      </table>
-    </div>
-  );
-}
-
 const Button = (props) => <button onClick={props.handleClick}>{props.text}</button>
 
 const Buttons = (props) => {
@@ -41,39 +12,58 @@ const Buttons = (props) => {
   );
 }
 
+const StatisticRow = (props) => {
+  return (
+    <tbody>
+      <tr>
+        <th>{props.text}</th>
+        <td>{props.value} {props.sign}</td>
+      </tr>
+    </tbody>
+  );
+}
+
+const Statistics = ({ good, neutral, bad }) => {
+  const total = good + neutral + bad;
+  const average = Math.round(((good - bad) / total) * 10) / 10;
+  const positive = Math.round(((good * 100) / total) * 10) / 10;
+
+  if (total === 0) {
+    return <p>No feedback given</p>
+  }
+  return (
+    <div>
+      <table>
+        <StatisticRow text='good' value={good} />
+        <StatisticRow text='neutral' value={neutral} />
+        <StatisticRow text='bad' value={bad} />
+        <StatisticRow text='all' value={total} />
+        <StatisticRow text='average' value={average} />
+        <StatisticRow text='positive' value={positive} sign='%' />
+      </table>
+    </div>
+  );
+}
+
 const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
-  const [total, setTotal] = useState(0);
-  const [score, setScore] = useState(0);
-  const [average, setAverage] = useState(0);
-  const [positive, setPositive] = useState(0);
 
   const handleClick = (feedback) => () => {
-    const updatedTotal = total + 1;
-    let updatedGood = good;
-    let updatedScore = score;
-
     switch (feedback) {
       case 'good':
-        setGood(updatedGood += 1);
-        setScore(updatedScore += 1);
+        setGood(good + 1);
         break;
       case 'neutral':
         setNeutral(neutral + 1);
         break;
       case 'bad':
         setBad(bad + 1);
-        setScore(updatedScore -= 1);
         break;
       default:
         console.log('No match feedback');
     }
-
-    setTotal(updatedTotal);
-    setAverage(Math.round((updatedScore / updatedTotal) * 10) / 10);
-    setPositive(Math.round(((updatedGood * 100) / updatedTotal) * 10) / 10);
   }
 
   return (
@@ -89,9 +79,6 @@ const App = () => {
         good={good}
         neutral={neutral}
         bad={bad}
-        total={total}
-        average={average}
-        positive={positive}
       />
     </div>
   );
