e32291b5bc4b8bf1122eb7db8124855a6491ac84
Kole Chaicharee <kole.chaicharee@gmail.com>
2023-09-14 16:59:31 +0900

    Add more middlewares

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -24,13 +24,14 @@ mongoose.connect(config.mongoUrl)
 
 app.use(cors());
 app.use(express.json());
-
+app.use(middleware.requestLogger);
 app.use(middleware.tokenExtractor);
 
 app.use('/api/blogs', blogsRouter);
 app.use('/api/users', usersRouter);
 app.use('/api/login', loginRouter);
 
+app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
 
 module.exports = app;
--- part4/bloglist/utils/middleware.js
+++ part4/bloglist/utils/middleware.js
@@ -1,5 +1,18 @@
 const jwt  = require('jsonwebtoken');
 const User = require('../models/user');
+const logger = require('./logger');
+
+const requestLogger = (request, response, next) => {
+  logger.info('Method:', request.method);
+  logger.info('Path:  ', request.path);
+  logger.info('Body:  ', request.body);
+  logger.info('---');
+  next();
+};
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' });
+};
 
 const tokenExtractor = (request, response, next) => {
   const authorization = request.get('authorization');
@@ -34,4 +47,10 @@ const errorHandler = (error, request, response, next) => {
   next(error);
 };
 
-module.exports = { errorHandler, tokenExtractor, userExtractor };
+module.exports = { 
+  errorHandler,
+  tokenExtractor,
+  userExtractor,
+  requestLogger,
+  unknownEndpoint
+};
